e0f7d033c438fb9b948424435fcd72814ff843e8
Anastasia Botsula <nactya.botsula@gmail.com>
2023-09-30 20:19:17 +0300

    feat: implement upload images when loading window

--- image-gallery/script.js
+++ image-gallery/script.js
@@ -4,29 +4,30 @@ const gallery = document.querySelector('.gallery-grid');
 const galleryColumns = document.querySelectorAll('.gallery-column');
 
 const galleryColumnsArr = Array.prototype.slice.call(galleryColumns);
+
+// API
+API_KEY = "L9c4awkR67nohMc_2Bg86AKJlb8Q_PR4i9cmYm43INo";
+apiUrl = "https://api.unsplash.com/photos/?client_id="+API_KEY+"&per_page=18";
+searchUrl = "https://api.unsplash.com/search/photos/?client_id="+API_KEY+"&query=";
+
+imgUrls = [];
+
+window.onload = (event) => {
+    loadImg();
+}
+
 async function loadImg() {
-    const inputValue = document.querySelector('.input').value;
+
     try {
-        await fetch(`https://api.unsplash.com/photos?&per_page=18&client_id=L9c4awkR67nohMc_2Bg86AKJlb8Q_PR4i9cmYm43INo`).then(
+        await fetch(apiUrl).then(
         (res) =>
             res.json().then((data) => {
                 console.log(data);
                 if(data) {                    
-                    let imgArr = [];
-                    for (let pic = 0; pic < data.length; pic++) {
-                        const img = document.createElement('img');
-                        img.classList.add('gallery-img');
-                        img.src = `${data[pic].urls.small}`;
-                        img.alt = `image`;
-                        img.style.width = `100%`;
-                        imgArr.push(img);
-                    }
-                    for (let i = 0; i < imgArr.length; i++) {
-                        const countOfImgAtOneColumn = imgArr.length / galleryColumnsArr.length;
-                        if (i < countOfImgAtOneColumn) galleryColumnsArr[0].append(imgArr[i]);
-                        if (i >= countOfImgAtOneColumn && i < countOfImgAtOneColumn * 2) galleryColumnsArr[1].append(imgArr[i]);
-                        if (i >= countOfImgAtOneColumn * 2) galleryColumnsArr[2].append(imgArr[i]);
-                    }
+                    data.forEach(pic => {
+                        imgUrls.push(pic.urls.small);
+                    })
+                    showImages();
                 }
             })
         );
@@ -34,6 +35,23 @@ async function loadImg() {
         errorMessage.style.display = 'block';
         errorMessage.innerHTML = 'An error happened, try again later, please';
     }
+}
+
+// Show images on the display
+function showImages() {
+    imgUrls.forEach((url, index) => {
+        const img = document.createElement('img');
+        img.classList.add('gallery-img');
+        img.src = url;
+        img.alt = `image`;
+        img.style.width = `100%`;
+
+        for (let i = 0; i < galleryColumnsArr.length; i++) {
+            if ((index + 1) % 3 === 0) galleryColumnsArr[0].appendChild(img);
+            if ((index + 2) % 3 === 0) galleryColumnsArr[1].appendChild(img);
+            if ((index + 3) % 3 === 0) galleryColumnsArr[2].appendChild(img);
+        }
+    });
 }
 
 btn.addEventListener('click', loadImg);
